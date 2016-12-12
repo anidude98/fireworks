aeab1be4be5b9739cb8eed1a5952ece666f91435
anidude98 <anidude98@gmail.com>
2016-12-11 22:08:48 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,71 +1,54 @@
+var rockets = [];
+var explosions = [];
+
 function setup() {
-  createCanvas(400, 400);
-	rocket = new particle(width/2,height,0,-9, 50);
-	rocket.tSparks = [];
-	xplo = [];
+  	createCanvas(400, 400);
+	rockets.push(new Rocket(width/2,height, 0, -50, 90));
+	stroke(255);
 }
 
 function draw() {
-  background(220);
-	rocket.y += rocket.vy;
-	rocket.vy *= 0.99;
-	stroke(255,25,25);
+  	background(220);
 	
-	sUpdate(rocket);
-	rocket.d -= 1;
-	
-	if(rocket.d < 1){
-	  //console.log("boom");
-	  xplo.push(new explosion(rocket.x,rocket.y, 3))
-    
-	}
-	
-	if(xplo.length > 0){
-	for(i = 0; i < xplo[0].spark.length; i++){
-	  point(xplo[0].spark[i].x,xplo[0].spark[i].y)
-	  xplo[0].spark[i].x += xplo[0].spark[i].vx;
-	  xplo[0].spark[i].y += xplo[0].spark[i].vy;
-	  xplo[0].spark[i].vy + 1;
-	  
-	}
-	}
-}
-
-function particle(x,y,vx,vy,d){
-  this.x = x;
-  this.y = y;
-  this.vx = vx;
-  this.vy = vy;
-  this.d = d;
-}
-
-function sUpdate(r){
-  for(i = 0; i < random(10)+10; i++){
-	  r.tSparks.push(new particle(r.x,r.y,random(15)-7.5,10+random(5),15));
-	  
-	}
-  
-  for(i = 0; i < r.tSparks.length; i++){
-	  r.tSparks[i].d -= 1;
-	  r.tSparks[i].y += r.tSparks[i].vy;
-	  r.tSparks[i].x += r.tSparks[i].vx;
-	  r.tSparks[i].vy *= 0.75;
-	  r.tSparks[i].vx *= 0.75;
-	  
-	  point(r.tSparks[i].x,r.tSparks[i].y);
-	  
-	  if(r.tSparks[i].d < 1){r.tSparks.splice(i,1);}
-	}
-	
+	rockets[0].update();
 	
 }
 
-function explosion(x,y,f){
-	  this.x = x;
-	  this.y = y;
-	  this.f = f;
-	  this.spark = [];
-	  for(x = 0; x < 10 * f; x++ ){
-	    this.spark.push(new particle(x,y,random(5*f)-((5*f)/2),random(5*f)*-1,random(5)+5*f));
-	  }
-	}
+function Rocket(x,y,vx,vy,d){
+	this.x = x;
+	this.y = y; 
+	this.vx = vx;
+	this.vy = vy;
+	this.d = d;
+	
+	this.trail = [];
+	
+	this.update = function(){
+		for(i = 0; i < 5; i++){	this.trail.push(new Particle(this.x,this.y,random(10)-5,random(10)+30,20)); }
+		for(i = 0; i < this.trail.length; i++){
+			this.trail[i].x += this.trail[i].vx;
+			this.trail[i].y += this.trail[i].vy;
+			
+			this.trail[i].vx *= 0.75;
+			this.trail[i].vy *= 0.75;
+			
+			this.trail[i].d -= 1;
+			
+			
+			if(this.trail[i].d < 1){  this.trail.splice(i,1;  }
+		}
+		for(i = 0; i < this.trail.length; i++){
+			point(this.trail[i].x,this.trail[i].y)
+		}
+		
+	};
+}
+
+function Particle(x,y,vx,vy,d){
+	this.x = x;
+	this.y = y; 
+	this.vx = vx;
+	this.vy = vy;
+	this.d = d;
+}
+
